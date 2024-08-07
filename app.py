b32ad87e6ebcbabe1412bf0a23323c9163ab6fb6
RudraRajdev <rudra51951@gmail.com>
2024-08-07 21:52:02 +0530

    This project uses a pre-trained Simple RNN model to perform sentiment analysis on IMDB movie reviews, classifying them as positive or negative. The application is built using TensorFlow, Keras, and Streamlit for an interactive user interface.
    IMDB Movie Review Sentiment Analysis
    This project provides a sentiment analysis tool for IMDB movie reviews using a pre-trained Simple RNN model. It allows users to input movie reviews and get a prediction on whether the review is positive or negative.
    
    Overview
    The project consists of a Python script that uses TensorFlow and Keras for sentiment analysis, along with a Streamlit app to provide an interactive user interface. The model used is a Simple RNN trained on the IMDB dataset.
    
    Project Structure
    simple_rnn.h5: Pre-trained Simple RNN model for sentiment analysis.
    app.py: Main Python script that includes model loading, preprocessing functions, and the Streamlit app.

---
+++ app.py
@@ -0,0 +1,62 @@
+#Step:1 
+# Import all libraries
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential,load_model
+from tensorflow.keras.layers import Embedding,SimpleRNN,Dense
+from tensorflow.keras.utils import pad_sequences
+
+#Load imdb datset word index
+
+word_index=imdb.get_word_index()
+reverse_index={value:key for key,value in word_index.items() }
+
+#Load pretrained model
+
+model=load_model('simple_rnn.h5')
+
+#Step:2 
+#Helper function
+
+#Function to decode reviews
+
+def decode_reviews(encoded_review):
+    return ' '.join([reverse_index.get(i-3,'?')for i in encoded_review])
+
+#Function for preprocess user output
+
+def preprocess_text(text):
+    words=text.lower().split()
+    encoded_review=[word_index.get(word,2)+3 for word in words]
+    padded_review=sequence.pad_sequences([encoded_review],maxlen=500)
+    return padded_review
+
+#Prediction function
+
+def predict_sentiment(review):
+    preprocess_input = preprocess_text(review)
+    prediction = model.predict(preprocess_input)
+    
+    sentiment='Positive' if prediction[0][0]>0.5 else 'Nagative'
+    
+    
+    return sentiment, prediction[0][0]
+
+#Streamlit app
+
+import streamlit as st
+st.title(" IMDB Movie review analysis")
+user_input=st.text_area("Movie review")
+if st.button("Classify"):
+    preprocess_input=preprocess_text(user_input)
+    
+        #Make prediction
+    pred=model.predict(preprocess_input)
+    sentiment='Positive' if pred[0][0]>0.5 else 'Nagative'
+
+    st.write(f'Sentiment:{sentiment}')
+    st.write(f'Prediction score:{pred[0][0]}')
+    
